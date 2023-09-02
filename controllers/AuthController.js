c7da70fcbec07547a2e5804f16d5c8965eec80fa
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-09-02 02:13:57 +0100

    working on task 5, not yet done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -11,18 +11,18 @@ export const getConnect = async (req, res) => {
 
 	const email = decodedToken.split(":")[0]
 
-	const user = await dbclient.findOne('users', { email})
+	const user = await dbclient.findOne('users', { email })
 
 	if (!user) return res.status(401).send({ error: "Unauthorized" })
 
-	const sessionToken = uuid.v4()	
+	const sessionToken = uuid.v4()
 	const key = `auth_${sessionToken}`
 
 	await redisClient.set(key, user._id.toString(), 24 * 60 * 60)
 
 	res.setHeader('x-token', key)
 
-	res.send({token: `${sessionToken}`})
+	res.send({ token: `${sessionToken}` })
 }
 
 export const getDisconnect = async (req, res) => {
@@ -32,7 +32,7 @@ export const getDisconnect = async (req, res) => {
 
 	const user = await dbclient.findById('users', userId)
 
-	if (!user) return res.status(401).send({error: "Unauthorized"})
+	if (!user) return res.status(401).send({ error: "Unauthorized" })
 
 	await redisClient.del(`auth_${sessionKey}`)
 
