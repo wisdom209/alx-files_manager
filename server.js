2965b002b0c4630451e9cd1de00a4f532361ef37
TessyJames28 <tessyjames28@gmail.com>
2023-09-02 15:46:22 +0100

    update

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 5000;
 
-app.use(bodyParser());
 app.use(router);
 
 app.listen(port, 'localhost', () => {
