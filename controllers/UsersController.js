a053206feb37f416049e1938a00428146166c802
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-09-02 22:31:42 +0100

    update

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,7 +5,7 @@ async function postNew(req, res) {
   const { email } = req.body;
   const { password } = req.body;
 
-  if (!email) return res.status(400).send({ errpr: 'Missing email' });
+  if (!email) return res.status(400).send({ error: 'Missing email' });
   if (!password) return res.status(400).send({ error: 'Missing password' });
 
   const emailExists = await dbClient.findOne('users', { email });
